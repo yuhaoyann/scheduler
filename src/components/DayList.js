f9a7f7e1035518d205f7ac0a2557bb7a68c44a43
yuhaoyann <yuhaoyann@gmail.com>
2021-11-18 22:10:52 -0500

    build up DayList and render to DayListItem and button

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList (props) {
+
+  const items = props.days.map(item => {
+    return (
+      <DayListItem
+        key={item.id}
+        name={item.name}
+        spots={item.spots}
+        selected={item.name === props.day}
+        setDay={props.setDay}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {items}
+    </ul>
+  )
+}
