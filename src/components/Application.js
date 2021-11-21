2481d79012fba4e41a26f85b65d079ef6893fbce
yuhaoyann <yuhaoyann@gmail.com>
2021-11-21 12:46:36 -0500

    render appointment view in application using static data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
@@ -48,7 +89,17 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {
+          Object.values(appointments).map((appointment) => {
+            return (
+              <Appointment
+                key={appointment.id} 
+                {...appointment} 
+              />
+            )
+          })
+        }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
