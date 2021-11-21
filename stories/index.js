60154e47bf715f64d464e08a4edcb2ff91c7c07a
yuhaoyann <yuhaoyann@gmail.com>
2021-11-20 21:38:47 -0500

    add form element to appointment form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -170,5 +171,21 @@ storiesOf("Button", module)
           <Error
             message="Could not delete appointment"
             onClose={action("onClose")}
+          />
+        ))
+        .add("Create", () => (
+          <Form
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />
+        ))
+        .add("Edit", () => (
+          <Form
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+            student={"Haoyan Yu"}
+            interviewer={3}
           />
         ))
