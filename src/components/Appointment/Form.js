610fbf18525099f7dbc3217d848217fdc26002c4
yuhaoyann <yuhaoyann@gmail.com>
2021-11-20 23:29:29 -0500

    add hook and state to eventhandlers in appointment form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form (props) {
 
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.student && props.student}
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
-          value={props.interviewer}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
