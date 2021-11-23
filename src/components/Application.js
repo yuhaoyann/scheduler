435a8fba34065db7a88fdd4a5df5a5d290b36445
yuhaoyann <yuhaoyann@gmail.com>
2021-11-23 15:08:34 -0500

    group state into object

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,8 +47,13 @@ const appointments = {
 
 export default function Application(props) {
   
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState((prev) => ({ ...prev, days}))
 
   useEffect(() => {
     axios.get("/api/days").then((response) => {
@@ -67,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
