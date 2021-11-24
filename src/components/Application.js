fe6bf3ca6749900c36f907b726e7c6b01dcf3028
yuhaoyann <yuhaoyann@gmail.com>
2021-11-23 20:48:38 -0500

    add function to fetch data from days and appointments API

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,45 +5,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   
@@ -53,13 +15,18 @@ export default function Application(props) {
     appointments: {}
   });
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState((prev) => ({ ...prev, days}))
 
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setDays(response.data)
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
     })
   }, [])
+
+  let dailyAppointments = [];
+  dailyAppointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -85,7 +52,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {
-          Object.values(appointments).map((appointment) => {
+          dailyAppointments.map((appointment) => {
             return (
               <Appointment
                 key={appointment.id} 
