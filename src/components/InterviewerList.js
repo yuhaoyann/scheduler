142a3edee6d81833518ccc59f2f2eaf159f08f09
yuhaoyann <yuhaoyann@gmail.com>
2021-12-18 02:41:21 -0500

    use prop-types to test if interviewers is array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 
 import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList (props) {
+function InterviewerList (props) {
 
   const interviewers = props.interviewers.map(interviewer => {
     return (
@@ -26,3 +27,10 @@ export default function InterviewerList (props) {
     </section>
   )
 }
+
+InterviewerList.propTypes={
+  interviewers: PropTypes.array.isRequired
+}
+
+export default InterviewerList;
+
