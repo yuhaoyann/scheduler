30759317cb578691ff9b714d97e3b04f20e2dfc9
yuhaoyann <yuhaoyann@gmail.com>
2021-12-30 22:03:25 -0500

    add neglify config file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -18,9 +18,9 @@ export default function useApplicationData () {
   });
   
   useEffect(() => {
-    // const webSocket = new WebSocket("process.env.REACT_APP_WEBSOCKET_URL");
+    const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
     // for testing use below
-    const webSocket = new WebSocket("ws://localhost:8001");
+    // const webSocket = new WebSocket("ws://localhost:8001");
     webSocket.onmessage = function (event) {
       dispatch({
         ...JSON.parse(event.data)
